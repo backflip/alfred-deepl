ab45affc2f383344f5eb4c5c54b267e7701c4022
Thomas Jaggi <thomas@responsive.ch>
2021-07-19 12:45:29 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const alfy = require("alfy");
+
+const apiKey = alfy.userConfig.get("apiKey");
+
+const { translations } = await alfy.fetch(
+  `https://api-free.deepl.com/v2/translate`,
+  {
+    query: {
+      auth_key: apiKey,
+      text: alfy.input,
+      target_lang: "EN",
+      source_lang: "DE",
+    },
+  }
+);
+
+const items = translations.map((element) => ({
+  title: element.text,
+}));
+
+alfy.output(items);
