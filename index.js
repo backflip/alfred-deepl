f9aa9d74527c33409b33d29ffd6ba8eac9f49537
Thomas Jaggi <thomas@responsive.ch>
2026-03-12 12:18:41 +0100

    fix: update api client

--- index.js
+++ index.js
@@ -2,19 +2,32 @@ import alfy from "alfy";
 
 const apiKey = alfy.userConfig.get("apiKey");
 
-const { translations } = await alfy.fetch(
-  `https://api-free.deepl.com/v2/translate`,
-  {
-    searchParams: {
-      auth_key: apiKey,
-      text: alfy.input,
-      target_lang: "EN",
-      source_lang: "DE",
-    },
-  }
-);
+let error = false;
 
-const items = translations.map((element) => ({
+const response = await fetch(`https://api-free.deepl.com/v2/translate`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `DeepL-Auth-Key ${apiKey}`,
+  },
+  body: JSON.stringify({
+    text: [alfy.input],
+    target_lang: "EN",
+    source_lang: "DE",
+  }),
+}).then((response) => {
+  if (!response.ok) {
+    error = true;
+  }
+
+  return response.json();
+});
+
+if (error) {
+  throw new Error(JSON.stringify(response));
+}
+
+const items = response.translations.map((element) => ({
   title: element.text,
   arg: element.text,
 }));
